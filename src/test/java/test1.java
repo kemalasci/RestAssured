c6865bdc2bad85e6697478da13671153ac85f018
ismettechno <ismettechno@gmail.com>
2020-11-25 19:08:27 +0000

    gitHub test 4

--- src/test/java/test1.java
+++ src/test/java/test1.java
@@ -2,5 +2,6 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("Öğretmen modeli");
         System.out.println("işleme devam ettim");
+        System.out.println("İşleme devam 2");
     }
 }
